3ed2a3b73e2596d2daed7adf583b1173805545fe
Ishita Gupta <124298301+ishitagupta09@users.noreply.github.com>
2023-06-15 22:30:03 +0530

    Create Java End-of-file.java

---
+++ Java End-of-file.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=1;
+        while (sc.hasNext()){
+            String b=sc.nextLine();
+            System.out.println(a+ " " + b);
+            a++;
+        }
+    }
+}
