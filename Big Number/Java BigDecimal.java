c2e3c3da25e0a31f1678fb7306825943bb05c039
Ishita Gupta <124298301+ishitagupta09@users.noreply.github.com>
2023-06-23 23:14:34 +0530

    Create Java BigDecimal.java

---
+++ Big Number/Java BigDecimal.java
@@ -0,0 +1,31 @@
+import java.math.BigDecimal;
+import java.util.*;
+class Solution{
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+          sc.close();
+for(int i=0;i<n;i++){
+    BigDecimal First=new BigDecimal(s[i]);
+    int index=i;
+    for(int j=i+1;j<n;j++){
+        BigDecimal Second=new BigDecimal(s[j]);
+        if(Second.compareTo(First)==1){
+            First=Second;
+            index=j;
+        }
+    }
+        String temp=s[i];
+        s[i]=s[index];
+        s[index]=temp;
+}
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
